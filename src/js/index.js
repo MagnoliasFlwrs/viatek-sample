abc68f272ad25e0ef3450fe565eeb819fb951725
martasavenok <magnolia.sav@gmail.com>
2024-11-13 01:25:03 +0300

    end

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,46 @@
+const phoneSelect = document.querySelector('.phone-select .header-phones');
 
+if (phoneSelect) {
+    phoneSelect.addEventListener('click' , (e)=> {
+        e.stopPropagation()
+        let currentSelectWrap = e.target.closest('.phone-select');
+        let currentSelectBody = currentSelectWrap.querySelector('.phone-select-wrapper');
+        currentSelectBody.classList.toggle('active');
+        phoneSelect.classList.toggle('active');
+    })
+    document.addEventListener('click', (e)=> {
+        let lists = document.querySelectorAll('.phone-select-wrapper.active')
+        if (!e.target.closest('.phone-select') && lists){
+            lists.forEach(el=> {
+                el.classList.remove('active');
+                phoneSelect.classList.toggle('active');
+            })
+        }
+    })
+}
+
+// menu
+
+const menuModal = document.querySelector('.menu-modal');
+const overlay = document.querySelector('.overlay');
+const closeMenuBtn = document.querySelector('.menu-modal .close-btn');
+const burgerBtn = document.querySelector('.burger-btn');
+
+const showMenuModal = () => {
+    overlay.classList.add('open');
+    menuModal.classList.add('active');
+}
+const hideMenuModal = () => {
+    overlay.classList.remove('open');
+    menuModal.classList.remove('active');
+}
+burgerBtn?.addEventListener('click' , ()=> {
+    showMenuModal()
+})
+
+closeMenuBtn?.addEventListener('click' , ()=> {
+    hideMenuModal()
+})
+overlay?.addEventListener('click' , ()=> {
+    hideMenuModal()
+})
